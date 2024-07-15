0e7fbc28eeba163487fa46d2065b3071c27507db
Nguyen Duc Thang <aloducthang@gmail.com>
2024-07-15 11:40:28 +0700

    feat: admin dashboard

--- src/pages/admin/Dashboard/index.jsx
+++ src/pages/admin/Dashboard/index.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import "./Dashboard.scss";
+import Users from './Components/Users';
+import Partners from './Components/Partners';
+import Hotels from './Components/Hotels';
+import Bookings from './Components/Bookings';
+import Packages from './Components/Packages';
 
 const Dashboard = () => {
     return (
-        <div>Dashboard</div>
+        <div className='admin-dashboard-wrapper'>
+            <div className='stats'>
+                <Users />
+                <Partners />
+                <Hotels />
+                <Bookings />
+            </div>
+            <Packages />
+        </div>
     )
 }
 
